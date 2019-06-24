8496fdf54f68d5bdd0c74cfd441d817491772924
Martin Tomes <tomesm@gmail.com>
2019-06-24 17:40:28 +0200

    Added hardcoded reposlugs

--- glabel/cmd.py
+++ glabel/cmd.py
@@ -4,31 +4,34 @@ import sys
 
 from .glabel import Glabel
 
+
+@click.command()
+@click.option('-a', '--config-auth', default='./auth.cfg',
+              help='File with authorization configuration.', metavar='FILENAME',
+              type=click.Path(exists=True))
+@click.option('-l', '--config-labels', default='./labels.cfg', metavar='FILENAME',
+              type=click.Path(exists=True))
+@click.option('-s', '--state', default='open', help='Filter pulls by state. [default: open]',
+              type=click.Choice(['open', 'closed', 'all']), required=False)
+@click.option('-b', '--base', help='Filter pulls by base (PR target) branch name.',
+              metavar='BRANCH', required=False)
+@click.option('-d', '--delete-old', default=True, help='Delete labels that do not match anymore.',
+              required=False)
+@click.argument('reposlugs', nargs=-1, required=False)
+
+def run(config_auth, config_labels, state, base, delete_old, reposlugs):
+    """ Run terminal labeler """
+    token = get_credentials(config_auth)
+    lbl = Glabel(token)
+    lbl.read()
+
+
 def get_credentials(file):
     """ Derives credentials form an auth file
-        :param: a config file with auth keys
+    :param: a config file with auth keys
         :return: config list with twitter credentials
     """
-    lbl.read()
     config = configparser.ConfigParser()
     config.read(file)
     return config['github']['token']
 
-@click.command()
-@click.option('-a', '--config-auth', default='./auth.cfg',
-                help='File with authorization configuration.', metavar='FILENAME',
-                type=click.Path(exists=True))
-@click.option('-l', '--config-labels', default='./labels.cfg', metavar='FILENAME',
-                type=click.Path(exists=True))
-@click.option('-s', '--state', default='open',
-                help='Filter pulls by state. [default: open]',
-                type=click.Choice(['open', 'closed', 'all']))
-@click.option('-b', '--base', help='Filter pulls by base (PR target) branch name.', metavar='BRANCH')
-@click.option('d', '--delete-old', default=True, help='Delete labels that do not match anymore.')
-
-
-def run(config_file):
-    """ Run terminal labeler """
-    token = get_credentials(config_file)
-    lbl = Glabel(token)
-    lbl.read()
--- glabel/glabel.py
+++ glabel/glabel.py
@@ -2,22 +2,31 @@ import requests
 import base64
 import time
 
+OWNER = '/tomesm'
+BASE_URL = 'https://api.github.com'
+REPOS = '/repos'
+REPO = '/labely-test'
+
 
 class Glabel:
     ''' Class for running the github labeler logic '''
 
     def __init__(self, token, session=None):
         ''' Class constructor initializes a session and last ID'''
-        self.page = requests.get('https://api.github.com/repos/tomesm/labely-test/pulls/1/files', headers = self.headers(token))
+        # self.page = requests.get('https://api.github.com/repos/tomesm/labely-test/pulls/1/files', headers = self.headers(token))
+        self.session = session or requests.Session()
+        self.set_session(token)
 
-    def headers(self, token):
+    def set_session(self, token):
         """ Gets headers
         :param token: GitHub personal access token
-        :return: HTTP headers for github request
         """
-        return {'Authorization': 'token' + token}
+        self.session.headers = {'User-Agent': 'Python'}
+        self.session.headers['Authorization'] = 'token' + token
 
     def read(self):
-        self.page.raise_for_status()
-        print(self.page.text)
+        url = BASE_URL + REPOS + OWNER + REPO + '/pulls'
+        response = self.session.get(url)
+        response.raise_for_status()
+        print(response.json())
 
