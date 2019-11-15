5271c2a018b7d1a2f411ba0da0a1b51ec0428813
Martin Tomes <tomesm@gmail.com>
2019-11-15 13:50:27 +0100

    Added requirements and config handling

--- glabel/cmd.py
+++ glabel/cmd.py
@@ -5,34 +5,35 @@ import sys
 from .glabel import Glabel
 from .api import API
 
-@click.command()
-@click.option('-a', '--config-auth', default='./auth.cfg',
-              help='File with authorization configuration.', metavar='FILENAME',
-              type=click.Path(exists=True))
-@click.option('-l', '--config-labels', default='./labels.cfg', metavar='FILENAME',
-              type=click.Path(exists=True))
-@click.option('-s', '--state', default='open', help='Filter pulls by state. [default: open]',
-              type=click.Choice(['open', 'closed', 'all']), required=False)
-@click.option('-b', '--base', help='Filter pulls by base (PR target) branch name.',
-              metavar='BRANCH', required=False)
-@click.option('-d', '--delete-old', default=True, help='Delete labels that do not match anymore.',
-              required=False)
-@click.argument('reposlugs', nargs=-1, required=False)
-
-def run(config_auth, config_labels, state, base, delete_old, reposlugs):
-    """ Run terminal labeler """
-    token = get_credentials(config_auth)
-    api = API(token)
-    lbl = Glabel(api, reposlugs)
-    lbl.read()
-
 
 def get_credentials(file):
     """ Derives credentials form an auth file
-    :param: a config file with auth keys
+        :param: a config file with auth keys
         :return: config list with twitter credentials
     """
     config = configparser.ConfigParser()
     config.read(file)
     return config['github']['token']
 
+
+@click.command()
+@click.option('-a', '--auth', default='./auth.cfg',
+              help='File with authorization configuration.', metavar='FILENAME',
+              type=click.Path(exists=True))
+@click.option('-c', '--config', default='./config.cfg', metavar='FILENAME',
+              type=click.Path(exists=True))
+@click.option('-s', '--state', default='open', help='Filter pulls by state. [default: open]',
+              type=click.Choice(['open', 'closed', 'all']), required=False)
+@click.option('-b', '--base', default='master', help='Filter pulls by base (PR target) branch name.',
+              metavar='BRANCH', required=False)
+@click.option('-d', '--delete-old', default=True, help='Delete labels that do not match anymore.',
+              required=False)
+@click.argument('reposlugs', nargs=-1, required=False)
+
+def run(auth, config, state, base, delete_old, reposlugs):
+    """ Run terminal labeler """
+    token = get_credentials(auth)
+    api = API(token)
+    lbl = Glabel(api, config, reposlugs)
+
+    lbl.read()
--- glabel/glabel.py
+++ glabel/glabel.py
@@ -1,28 +1,58 @@
 import fnmatch
+import configparser
+
+def get_keys(file, section):
+    parser = configparser.ConfigParser()
+    parser.read(file)
+    return list(parser[section].keys())
+
+
+def set_parser(file, section):
+    parser = configparser.ConfigParser()
+    parser.read(file)
+
+    d = {}
+
+    for key in list(parser[section].keys()):
+        confs = []
+
+        for str in parser[section][key]:
+            confs.append(str)
+
+        d[key] = confs
+
+    return d
+
 
 class Glabel:
     ''' Class for running the github labeler logic '''
 
-    def __init__(self, api, reposlugs):
+    def __init__(self, api, config, reposlugs):
         ''' Class constructor initializes a session and last ID'''
         self.reposlugs = reposlugs
         self.api = api
+        self.keys = get_keys(config, 'labels')
+        self.values = set_parser(config, 'labels')
 
-    def handle_slug(self, slug):
-        owner = slug.split('/')[0]
-        repo = slug.split('/')[1]
+        print(self.values)
+
+    def handle_slugs(self, slugs):
+        owner = slugs[0]
+        repo = slugs[1]
         return '/' + owner, '/' + repo
 
 
     def read(self):
-        owner, repo = self.handle_slug(self.reposlugs[0])
+        owner, repo = self.handle_slugs(self.reposlugs)
         pull_number = self.api.get_pull_number(owner, repo)
         files = self.api.get_pull_files(owner, repo, pull_number)
-        print(files[0]['status'])
+        # print(files[0]['status'])
 
         if files[0]['status'] == 'modified':
             filename = files[0]['filename']
+
             label = filename.split('/')[0]
-            if fnmatch.fnmatch(label, '*/templates/*'):
+
+            if fnmatch.fnmatch(label, '*templates*'):
                 print(label)
 
