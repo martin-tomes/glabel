6da39a69a4546caad22819165220395025d5b7fc
Martin Tomes <tomesm@gmail.com>
2019-12-05 17:14:09 +0100

    Added refactoring

--- glabel/glabel.py
+++ glabel/glabel.py
@@ -1,19 +1,17 @@
 import fnmatch
 import configparser
 
-def get_keys(file, section):
-    parser = configparser.ConfigParser()
-    parser.read(file)
-    return list(parser[section].keys())
-
 
 def parse_config(file, section):
     parser = configparser.ConfigParser()
     parser.read(file)
     parsed = {}
+
     for key in list(parser[section].keys()):
         strings = parser[section][key]
         parsed[key] = strings.split('\n')
+
+    # print(parsed)
     return parsed
 
 
@@ -25,31 +23,42 @@ class Glabel:
         self.reposlugs = reposlugs
         self.api = api
         self.configs = parse_config(config, 'labels')
-        # print(self.values)
-        for section in self.configs.values():
-            print(section)
-            match = [v for v in section if fnmatch.fnmatch('templates', v)]
-            print(match)
+
+        # for section in self.configs.values():
+            # print(section)
+            # match = [v for v in section if fnmatch.fnmatch('templates', v)]
+            # print(match)
+
 
     def handle_slugs(self, slugs):
         owner = slugs[0]
         repo = slugs[1]
+
         return '/' + owner, '/' + repo
 
 
     def read(self):
         owner, repo = self.handle_slugs(self.reposlugs)
         pull_number = self.api.get_pull_number(owner, repo)
-        files = self.api.get_pull_files(owner, repo, pull_number)
-        # print(files[0]['status'])
+        response = self.api.get_pull_files(owner, repo, pull_number)
+        self.handle_response(response)
 
-        if files[0]['status'] == 'modified':
-            filename = files[0]['filename']
 
-            label = filename.split('/')[0]
+    def handle_response(self, response):
+        for section in response:
+            if section['status'] == 'modified':
+                filename = section['filename']
+                label = filename.split('/')[0]
 
-            for value in self.configs.values():
-                print(value)
-                # if fnmatch.fnmatch(label, '*templates*'):
-                    # print(label)
+                for key, value in self.configs.items():
+                    if self.is_match(value, label):
+                        print(key)
+
+
+    def is_match(self, value, label):
+        for item in value:
+            if fnmatch.fnmatch(label, item):
+                return True
+        return False
+
 
